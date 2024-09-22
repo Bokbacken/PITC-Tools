dfbe466cda48f75d4f1a0fe8a9d4402d59ce6afc
Bokbacken <69871284+Bokbacken@users.noreply.github.com>
2024-09-22 21:55:15 +0200

    Update utilities.sh

--- utilities.sh
+++ utilities.sh
@@ -5,6 +5,6 @@ sudo apt install -y network-manager python3 git iproute2 python-flask
 sudo apt install iperf3 wireshark
 sudo usermod -a -G wireshark pi
 sudo cp /home/pi/PITC-Tools/Bridge/br-start.sh /etc/init.d/brstart
-sudo update-rc.d br-start.sh defaults
+sudo update-rc.d brstart defaults
 sudo nano /etc/init.d/brstart
 sudo cp /home/pi/PITC-Tools/manipulate/manipulate.desktop /usr/share/applications/manipulate.desktop
